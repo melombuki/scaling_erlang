9e58e3211b206e943ad0b520f5ef373d9c44bd7c
Josh Hemen <melombuki@gmail.com>
2018-07-17 20:22:59 -0500

    Add phone supervisor

---
+++ src/phone_sup.erl
@@ -0,0 +1,34 @@
+-module(phone_sup).
+-behaviour(supervisor).
+
+-export([start_link/0, attach_phone/1, detach_phone/1]).
+-export([init/1]).
+
+start_link() ->
+  supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+init([]) ->
+  {ok, {{one_for_one, 10, 3600}, []}}.
+
+attach_phone(Ms) ->
+  case hlr:lookup_id(Ms) of
+    {ok,  _Pid} ->
+      {error, attached};
+    _NotAttached ->
+      ChildSpec = #{id => Ms,
+                    start => {phone_statem, start_link, [Ms]},
+                    restart => transient,
+                    shutdown => 2000,
+                    type => worker,
+                    modules => [phone_statem]},
+      supervisor:start_child(?MODULE, ChildSpec)
+  end.
+
+detach_phone(Ms) ->
+  case hlr:lookup_id(Ms) of
+    {ok, _Pid} ->
+      supervisor:terminate_child(?MODULE, Ms),
+      supervisor:delete_child(?MODULE, Ms);
+    _NotAttached ->
+      {error, detached}
+  end.
